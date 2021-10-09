76ceb1b0cfdb017dcffa23d86862961172342f6e
Megastarbot <89236767+Megastarbot@users.noreply.github.com>
2021-10-09 20:30:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -192,8 +192,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            
+            InlineKeyboardButton('Source Code', url='https://t.me/MalluSearchBot')
         ]
         ]
     await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
